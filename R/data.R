9e8ebd9804791a938c06e48f295d06035d3d3432
boydpe <boydpe@oregonstate.edu>
2021-03-08 23:29:04 -0800

    data update

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Hector Mine earthquake catalog.
+#'
+#' A dataset containing the location, magnitude, and time since initial earthquake
+#' event occurring on  October 16, 1999.
+#'
+#' @format A data frame with 542 rows and 4 variables:
+#' \describe{
+#'   \item{lon}{longitude}
+#'   \item{lat}{latitude}
+#'   \item{t}{time since the initial earthquake event, measured in days}
+#'   \item{m}{magnitude}
+#' }
+#' @source Hector mine data courtesy of the U.S. Geological Survey
+"hm"
